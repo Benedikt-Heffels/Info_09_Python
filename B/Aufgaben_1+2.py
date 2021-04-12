b28d46c6386f380de94204b19bc435f8bf78c993
Benedikt-Heffels <benedikt.heffels@gmail.com>
2021-04-12 10:30:41 +0200

    07_B (For-Schleife) Aufgaben

---
+++ B/Aufgaben_1+2.py
@@ -0,0 +1,13 @@
+liste = ["Dies", "ist", "eine", "Liste", "voller", "Wörter"]
+
+for w in liste:
+    print(w)
+
+print("\n\n------------------------------------------Neuer Teil :)-----------------------------------------------\n\n")
+
+satz = "Das ist das Haus vom Nikolaus"
+for buchstabe in satz:
+    print(buchstabe)
+
+print("\n\n------------------------------------------Neuer Teil :)-----------------------------------------------\n\n")
+
