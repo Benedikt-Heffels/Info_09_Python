82cd589d9d078f65527c8e769a957e102a5071b0
Benedikt-Heffels <benedikt.heffels@gmail.com>
2021-06-17 18:29:27 +0200

    Tetris - Einbidung der Tastatur

--- _08-__Pygame/Tetris/Tetris_003.py
+++ _08-__Pygame/Tetris/Tetris_003.py
@@ -5,11 +5,18 @@ import random
 block_colors = [(0, 255, 255), (255, 255, 0), (128, 0, 128), (0, 255, 0), (255, 0, 0), (0, 0, 255), (255, 127, 0)]
 
 
+
+def neuer_spielstein():
+    global random_color, block_x, block_y, block_speed
+    pygame.draw.rect(win, block_colors[random_color], (block_x, block_y + block_speed, block_size, block_size))
+
+
+
 block_size = 20
 board_width = block_size * 16
 board_height = block_size * 20
 block_speed = 5
-block_x = random.randint(0, 380)
+block_x = block_size * 16 / 2
 block_y = 50
 random_color = random.randint(0, 6)
 
@@ -25,13 +32,26 @@ while bRun:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             bRun = False
+    gedrueckt = pygame.key.get_pressed()
 
+    if gedrueckt[pygame.K_DOWN]:
+        block_speed += 100
+    elif gedrueckt[pygame.K_LEFT]:
+        if block_x >= 10:
+            block_x -= 10
+        else:
+            print("Sie haben das Ende des Spielfeldes erreicht!")
+    elif gedrueckt[pygame.K_RIGHT]:
+        if block_x <= board_width - 30:
+            block_x += 10
+        else:
+            print("Sie haben das Ende des Spielfeldes erreicht!")
 
-    pygame.draw.rect(win, block_colors[random_color], (block_x, block_y + block_speed, block_size, block_size))
+    neuer_spielstein()
     block_speed += 5
-    if block_speed >= block_size * 20 - 70:
+    if block_speed >= board_height - 70:
         block_speed = 0
         random_color = random.randint(0, 6)
-        block_x = random.randint(0, 380)
+        block_x = block_size * 16 / 2
 
     pygame.display.update()
